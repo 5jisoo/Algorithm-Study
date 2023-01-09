bae7b0db29713fb7bcabc2c334b0c92470c55115
Nyeong <iwbm312@gmail.com>
2023-01-09 17:22:50 +0900

    [MOD] 11866 인덱스에러 수정
    1. 3%7==3이다. 즉, 굳이 7 넘어가는 놈들만 mod해주려고 조건을 따로 분기하지 않아도 됨.
    
    2. 따로 분기하다가 길이 <= 라고 해야하는데 길이< 라고 처리해서 틀림
    
    해결) 지금처럼 조건을 합치거나
    기존코드 #17줄에서 =를 추가해주면 통과

--- 23-winter-algorithm/stack_queue/baekjoon/11866/신진영.py
+++ 23-winter-algorithm/stack_queue/baekjoon/11866/신진영.py
@@ -2,24 +2,19 @@
 from sys import stdin
 
 N, K = map(int,stdin.readline().split())
+n = N
 ppl = []
 yose = []
 
-for i in range(N):
-  ppl.append(i+1)
+ppl = [i+1 for i in range(N)]
 
 idx = -1
-while len(ppl) > 0:
-  if len(ppl) == 1:
-    yose.append(ppl[0])
-    break
-  idx += K
-  if idx > len(ppl):
-    idx %= len(ppl)
+while n:
+  idx = (idx + K) % n
   num = ppl.pop(idx)
   idx -= 1
   yose.append(num)
-
+  n -= 1
 
 print("<", end="")
 for j in range(N):
