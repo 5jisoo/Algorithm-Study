5cbfadd187df5706fc8cfb9d1cb3f2f880f30c21
Nyeong <iwbm312@gmail.com>
2023-01-23 14:30:29 +0900

    [CORRECT] 게임 개발 - 변수명 변경

--- 23-winter-algorithm/implementation/게임 개발/신진영.py
+++ 23-winter-algorithm/implementation/게임 개발/신진영.py
@@ -1,10 +1,11 @@
+# 이 코드 단점: 입력받은 m을 사용하지 않음
 n, m = map(int, input().split()) # map size
-x, y, view = map(int, input().split()) # position, view
+x, y, direction = map(int, input().split()) # position, direction
 map_lst = []
 ways = []
 # 처음 위치 추가
 ways.append((x, y))
-check_count = 0
+turn_time = 0
 
 # input map
 for i in range(n):
@@ -13,13 +14,13 @@ for i in range(n):
 # escape condition: check 4position but can't move, can't move 1 back
 # 현재 방향 기준으로 왼쪽방향 탐색
 while True:
-  if view == 0:
+  if direction == 0:
     tx, ty = x-1, y
-  elif view == 1:
+  elif direction == 1:
     tx, ty = x, y-1
-  elif view == 2:
+  elif direction == 2:
     tx, ty = x+1, y
-  elif view == 3:
+  elif direction == 3:
     tx, ty = x, y+1
 
   # 모든 조건 만족하면 이동
@@ -28,22 +29,22 @@ while True:
     x = tx
     y = ty
     ways.append((tx, ty)) # 이동한 위치 기록
-    check_count = 0
-  elif check_count < 3: # 아직 모든 방향 탐색 이전이라면 다음방향 확인위해 view변경
-    check_count += 1
-    view += 1
-    view %= 4
+    turn_time = 0
+  elif turn_time < 3: # 아직 모든 방향 탐색 이전이라면 다음방향 확인위해 direction변경
+    turn_time += 1
+    direction += 1
+    direction %= 4
     continue
-  elif check_count == 3: # 모든 방향 탐색하고도 이동하지 못했다면 기존방향대로 뒤로 한 칸 이동
-      view += 1
-      view %= 4
-      if view == 0:
+  elif turn_time == 3: # 모든 방향 탐색하고도 이동하지 못했다면 기존방향대로 뒤로 한 칸 이동
+      direction += 1
+      direction %= 4
+      if direction == 0:
         tx, ty = x, y+1
-      elif view == 1:
+      elif direction == 1:
         tx, ty = x-1, y
-      elif view == 2:
+      elif direction == 2:
         tx, ty = x, y-1
-      elif view == 3:
+      elif direction == 3:
         tx, ty = x+1, y
       
       if map_lst[tx][ty] == 1: # 뒤쪽 방향이 바다라면 break
@@ -51,5 +52,5 @@ while True:
       else: # 뒤로 이동할 수 있다면 이동
         x = tx
         y = ty
-        check_count = 0
+        turn_time = 0
 print(len(ways))
